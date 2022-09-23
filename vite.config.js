0e60504932dfcffd8eb528d0feb1cc644a298f3d
RafaelDubois <rafaeldubois974@gmail.com>
2022-09-23 10:14:10 +0200

    test font optimization

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import fontOptimizationPlugin from 'vite-plugin-font-optimization'
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(),fontOptimizationPlugin({providers: ['https://fonts.cdnfonts.com/',"https://fonts.cdnfonts.com/css/mythical-prince","https://fonts.cdnfonts.com/css/samarkan"],
+})],
   server: {
     port: 3000,
   },
